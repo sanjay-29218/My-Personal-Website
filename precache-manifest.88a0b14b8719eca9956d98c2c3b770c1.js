cf3032118eee236fa3dab89ce04823bf540729db
Sanjay Poudel <sanjay29218@gmail.com>
2022-06-22 16:23:18 +0545

    Updates

---
+++ precache-manifest.88a0b14b8719eca9956d98c2c3b770c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7888e9f6359c5b4f94a51d545148503",
+    "url": "/MyPersonalWebsite/index.html"
+  },
+  {
+    "revision": "6e75b1ff4ee7a83a40c3",
+    "url": "/MyPersonalWebsite/static/css/main.2044c8a7.chunk.css"
+  },
+  {
+    "revision": "15a2fe9ad89c592aaf6f",
+    "url": "/MyPersonalWebsite/static/js/2.68978b41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/MyPersonalWebsite/static/js/2.68978b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e75b1ff4ee7a83a40c3",
+    "url": "/MyPersonalWebsite/static/js/main.8bfa87e1.chunk.js"
+  },
+  {
+    "revision": "3ff08af1fd8c01a6f0bd",
+    "url": "/MyPersonalWebsite/static/js/runtime-main.ce24c292.js"
+  }
+]);
